c22b89f28317dd315d36a69a784250054922cfe7
BLooDBRothER <computertechnology446@gmail.com>
2021-09-03 18:05:48 +0530

    Add gray and random color function

--- main.js
+++ main.js
@@ -3,8 +3,17 @@ const slider = document.querySelector(".slider__input");
 const hover = document.querySelector(".hover");
 
 // Option button
+const optionCnt = document.querySelector(".option");
 const options = document.querySelectorAll(".option__each");
 let borderBool = false;
+
+function randNo(){
+    return Math.floor(Math.random() * 255);
+}
+
+function returnRGB(value){
+    return value.match(/rgb\(([0-9]*), ([0-9]*), ([0-9]*)\)/);
+}
 
 // option function
 function toggleBorders(){
@@ -14,12 +23,37 @@ function toggleBorders(){
     });
 }
 
+function calculateGray(elem){
+    if(!elem.dataset.gray){
+        elem.dataset.gray = "on";
+        elem.style.backgroundColor = `rgb(255, 255, 255)`;
+    }
+    let clr = returnRGB(elem.style.backgroundColor);
+    elem.style.backgroundColor = clr[1] <= 0 ? `rgb(255, 255, 255)` : `rgb(${clr[1]-25}, ${clr[1]-25}, ${clr[1]-25})`;
+}
+
+function applyColor(elem){
+    let color;
+    switch(optionCnt.dataset.value){
+        case "random":
+            color = `rgb(${randNo()}, ${randNo()}, ${randNo()})`;
+            break;
+        case "gray":
+            calculateGray(elem);
+            break;
+        case "invert":
+
+    }
+    elem.style.backgroundColor = color;
+    document.documentElement.style.setProperty("--curr-color", color);
+}
+
 // event listener
 function addListener(){
     let small = hover.querySelectorAll(".hover__small");
     small.forEach(each => {
         each.addEventListener("mouseenter", (e) => {
-            e.target.style.backgroundColor = "red";
+            applyColor(e.target);
         })
     });
 }
@@ -39,6 +73,7 @@ slider.addEventListener("input", (e) => {
     addListener();
 });
 
+let prevOption = options[2];
 options.forEach(option => {
     option.addEventListener("click", function(e) {
         if(this.classList.contains("option__border")){
@@ -46,6 +81,10 @@ options.forEach(option => {
             this.classList.toggle("active");
             toggleBorders();
         }
+        optionCnt.dataset.value = this.dataset.value;
+        prevOption.classList.remove("active");
+        this.classList.add("active");
+        prevOption = this;
     })
 });
 
